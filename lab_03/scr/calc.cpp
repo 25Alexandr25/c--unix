35cb634ff368f0ea05b22308b40334223ac1fe83
25Alexandr25 <64425763+25Alexandr25@users.noreply.github.com>
2025-06-05 17:06:52 +0300

    Update calc.cpp

--- lab_03/scr/calc.cpp
+++ lab_03/scr/calc.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath> // для pow()
 
 using namespace std;
 
@@ -11,6 +12,10 @@ double minusF(double a, double b) {
     return a - b;
 }
 
+//double powerF(double a, double b) {
+    //return pow(a, b);
+//}
+
 double powerF(double a, double b) {
     double result = 1;
     for (int i = 0; i < b; i++) {
@@ -19,28 +24,36 @@ double powerF(double a, double b) {
     return result;
 }
 
-int wmain(int argc, wchar_t *argv[]) {
-    double a = _wtoi(argv[1]);
-    double b = _wtoi(argv[3]);
+int main() {
+    double a, b;
+    string op;
+
+    // Ввод данных
+    cout << "Введите первое число: ";
+    cin >> a;
+    cout << "Введите оператор (+, -, ^, plus, minus, power): ";
+    cin >> op;
+    cout << "Введите второе число: ";
+    cin >> b;
+
     double result;
-    wstring operatorF = argv[2];
-    wstring plus(L"plus");
-    wstring minus(L"minus");
-    wstring power(L"power");
-    wstring plusS(L"+");
-    wstring minusS(L"-");
-    wstring powerS(L"^");
-    if (operatorF == plus or operatorF == plusS) {
+
+    // Выбор операции
+    if (op == "plus" || op == "+") {
         result = plusF(a, b);
-    } else if (operatorF == power or operatorF == powerS) {
-        result = powerF(a, b);
-    } else if (operatorF == minus or operatorF == minusS) {
-        result = minusF(a, b);
-    } else {
-        cout << "Invalid operator!\n";
-        return 0;
     }
-    cout << "Result: " << result << "\n";
-    cout << "Проверка";
+    else if (op == "minus" || op == "-") {
+        result = minusF(a, b);
+    }
+    else if (op == "power" || op == "^") {
+        result = powerF(a, b);
+    }
+    else {
+        cerr << "Ошибка: неизвестный оператор!" << endl;
+        return 1;
+    }
+
+    // Вывод результата
+    cout << "Результат: " << result << endl;
     return 0;
 }
